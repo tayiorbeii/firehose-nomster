fe27f4d8d43d3245ba4acc1653e027772acdebcf
Taylor Bell <taylorbell@gmail.com>
2017-11-03 10:42:36 +0000

    add form

--- Gemfile
+++ Gemfile
@@ -61,3 +61,5 @@ source 'https://rails-assets.org' do
 end
 
 gem 'will_paginate', '~> 3.1.0'
+
+gem 'simple_form'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,4 +5,18 @@ class PlacesController < ApplicationController
     @places = Place.paginate(:page => params[:page], :per_page => 1)
   end
 
+  def new
+    @place = Place.new
+  end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
